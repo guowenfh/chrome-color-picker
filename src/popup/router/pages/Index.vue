0de5098a3f86e4b3c0a75e3897cf8edac8639808
guowenfh <guowen921@live.com>
2018-11-08 01:16:07 +0800

    add vue-color

--- src/popup/router/pages/Index.vue
+++ src/popup/router/pages/Index.vue
@@ -1,15 +1,35 @@
 <template>
-  <p>Hello world!</p>
+  <div>
+    <p>Hello world!</p>
+
+    <chrome-picker v-model="colors" />
+    <button @click="click">截图</button>
+  </div>
 </template>
 
 <script>
 
+import { Chrome } from 'vue-color'
 export default {
+  components:{
+    'chrome-picker' : Chrome ,
+  },
   data () {
-    return {}
+    return {
+      colors:{ r: 255, g: 0, b: 0 }
+    }
   },
   mounted () {
     browser.runtime.sendMessage({})
+  },
+  methods:{
+    click(){
+      chrome.tabs.captureVisibleTab(null, {}, function (image) {
+        console.error('----',image)
+    // You can add that image HTML5 canvas, or Element.
+  });
+
+    }
   }
 }
 </script>
