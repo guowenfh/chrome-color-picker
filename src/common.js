ce9f2f80ef7ab2345eab75e077b032403e6a2b22
guowenfh <guowen921@live.com>
2018-11-10 23:35:42 +0800

    修复颜色取值范围不对的问题

--- src/common.js
+++ src/common.js
@@ -62,6 +62,13 @@ export function loadImage(url, callback, context, crossOrigin) {
   img.src = url
 }
 
+export function chunks(arr = [], size) {
+  const r = []
+  for (let i = 0; i < arr.length; i += size) {
+    r.push(arr.slice(i, i + size))
+  }
+  return r
+}
 export function clacToOffsetArray(base, offset = 1, isAdd = true) {
   return [...Array(offset + 1)].reduce((newArr, item, index) => {
     if (isAdd) {
@@ -85,8 +92,8 @@ export function getRectPoint(x, y, offset = 5) {
   const maxYPosArr = clacToOffsetArray(y, offset, true)
   const xPos = [...new Set(minXPosArr.concat(maxXPosArr))]
   const yPos = [...new Set(minYPosArr.concat(maxYPosArr))]
-  const matrix = xPos.map(x => {
-    return yPos.map(y => ({
+  const matrix = yPos.map(y => {
+    return xPos.map(x => ({
       x: x,
       y: y
     }))
--- src/content_scripts/App.vue
+++ src/content_scripts/App.vue
@@ -1,31 +1,25 @@
 <template>
   <div>
     <div :style="wrapStyle" id="color-picker-wrap" ref="pickWrap">
-      <div :style="rgbaArea">
-        <div v-for="(item, index) in matrix" :key="'label' + index">
-          <div v-if="item.length" style="width:100px;display:flex;">
-            <div
-              v-for="xyItem in item"
-              :key="xyItem.x + '-' + xyItem.y"
-              v-bind:style="{
-                backgroundColor: xyItem.backgroundColor,
-                height: '10px',
-                width: '10px',
-                border: `1px solid ${xyItem.isActive ? 'red' : '#ddd'}`
-              }"
-            ></div>
-          </div>
-        </div>
-      </div>
+      <table :style="rgbaArea">
+        <tr v-for="(item, index) in matrix" :key="'label' + index" style="display:flex;">
+          <td v-for="xyItem in item" :key="xyItem.x + '-' + xyItem.y" v-bind:style="{
+              backgroundColor: xyItem.backgroundColor,
+              height: '8px',
+              width: '8px',
+              boxShadow: `0 0px 0px 1px ${xyItem.isActive ? 'red' : '#ddd'}`,
+              zIndex: `${xyItem.isActive ? 1 : 0}`
+            }" />
+        </tr>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import { loadImage, getRectPoint } from '../common'
+import { loadImage, chunks } from '../common'
 
-function pixelToRgba(pixel) {
-  const data = pixel.data
+function pixelToRgba(data = []) {
   const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`
   return rgba
 }
@@ -47,11 +41,13 @@ export default {
       cursor:
         'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAXCAYAAADgKtSgAAAAAXNSR0IArs4c6QAAAblJREFUSA3dVMtKw1AQzWsj/oULA0GD+AFacGshSat7/Qw3uvIzdG8pCcStWPoBUlQodeFfFFdJPCfckds8JIouNFBm7syZM3PnztQ2On5RFF24rnvveZ45n88nXcKsLqDvYv45eRiGW3me77I9lDx3aZX5GWgwGGyA7Looir0qzjTNqWVZJ+Px+LXqk3MreRAExwBd4bcu4Aa5hO00SZKbBp/RSM6Ksyx7REBJjCrfoN/hBgvoLvQD6GuKcGnbtt90A0cBVgRbAYMQM8lRHMcvAkLPN5FkhAQ+cQq/L36Rtcr5WAh6IkBVvKMTSyATQJ/JDYDdBu5Z/JTmcDjsIXNPjNA5FX2eEXCLgFIXvy6RIAX5obKleOAH8UOfOCTG71yMukTgQj9XdeUX8j54VgpxmEEPqlTOx2v9cDMXCcRfq/xXe177b+GjoKIpy0FVHLeRejyp0FBnTks5jsRXH5PgxlHk5smcg8BH8AyErXNO/EdmTam1RXw/saG1tgg5V5qbJy0Su0ja6W9bfeJaKxcSSrSEi3UJlaOWgvisqcfE6l9r5TqIROhruSCUXYgZ34lcT/QV/e+SvwPjb9Vawp+KUwAAAABJRU5ErkJggg==") 16 16, crosshair;',
       rgbaArea: {
-        backgroundColor: 'rgba(255,255,255,0)',
+        borderRadius: '50%',
+        overflow: 'hidden',
+        borderCollapse: 'collapse',
+        boxShadow: '0 0px 0px 1px #4c4c4c',
+        pointerEvents: 'none',
         top: '-1px',
         position: 'absolute',
-        height: '100px',
-        width: '100px',
         left: '-1px',
         zIndex: '1111'
       },
@@ -69,30 +65,29 @@ export default {
       this.$refs.pickWrap.appendChild($canvas)
     },
     mousemove(ev) {
-      const x = ev.clientX
-      const y = ev.clientY
-
-      this.rgbaArea.top = `${y + 10}px`
-      this.rgbaArea.left = `${x + 10}px`
-      const matrix = getRectPoint(x, y, 5)
-      const list = matrix.map(arr => {
-        return arr.map(item => {
-          const isContains =
-            item.x > 0 &&
-            item.x < this.imgStyles.width &&
-            (item.y > 0 && item.y < this.imgStyles.height)
-          const pixel = this.ctx.getImageData(item.x, item.y, 1, 1)
+      const clientX = ev.clientX
+      const clientY = ev.clientY
+      this.rgbaArea.top = `${clientY + 10}px`
+      this.rgbaArea.left = `${clientX + 10}px`
+      const originX = clientX - 5
+      const originY = clientY - 5
+      const imageData = this.ctx.getImageData(originX, originY, 11, 11)
+      // 参考 https://developer.mozilla.org/zh-CN/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
+      // 每四个点表示一个 rgba 的值
+      const pixel = chunks(imageData.data, 4)
+      const rgbarr = chunks(pixel.map(item => pixelToRgba(item)), 11)
+      this.matrix = rgbarr.map((arr, yIndex) => {
+        return arr.map((rgba, xIndex) => {
+          const x = originX + xIndex
+          const y = originY + yIndex
           return {
-            ...item,
-            isActive: item.x === x && item.y === y,
-            backgroundColor: isContains
-              ? pixelToRgba(pixel)
-              : 'rgba(255,255,255,0)'
+            x,
+            y,
+            isActive: clientX === x && clientY === y,
+            backgroundColor: rgba
           }
         })
       })
-      this.matrix = list
-      console.error(list)
     },
     click() {
       this.$canvas.removeEventListener('mousemove', this.mousemove)
@@ -112,10 +107,11 @@ export default {
           this.rednerCanvas(this.$canvas, this.imgStyles)
           this.$canvas.style = `cursor:${this.cursor}`
           document.body.style = `cursor:${this.cursor}`
-          ;(this.wrapStyle.height = `${this.imgStyles.height}px`),
-            (this.wrapStyle.width = `${this.imgStyles.width}px`),
-            (this.wrapStyle.display = 'block'),
-            (this.ctx = this.$canvas.getContext('2d'))
+          this.wrapStyle.height = `${this.imgStyles.height}px`
+          this.wrapStyle.width = `${this.imgStyles.width}px`
+          this.wrapStyle.display = 'block'
+
+          this.ctx = this.$canvas.getContext('2d')
           this.ctx.drawImage(img, 0, 0)
           this.$canvas.addEventListener('mousemove', this.mousemove)
           this.$canvas.addEventListener('click', this.click)
